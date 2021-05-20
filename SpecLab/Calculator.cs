60ce4da79a4c3599528fc938e025b9d60dc563a3
Xawe <dreamxawe@hotmail.com>
2021-05-20 17:07:10 -0300

    .

--- SpecLab/Calculator.cs
+++ SpecLab/Calculator.cs
@@ -7,8 +7,6 @@ namespace SpecLab
         public int FirstNumber { get; set; }
         public int SecondNumber { get; set; }
 
-        
-
         public int Add()
         {
             return  FirstNumber + SecondNumber;            
